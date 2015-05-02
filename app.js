a171535145492f3dea36dc7f78b04ab5ec86539a
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-05-01 21:43:06 -0400

    link to api with JSON callback

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ weatherApp.service('cityService', function() {
 weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
 
 	$scope.city = cityService.city;
+	$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', {
+		callback:"JSON_CALLBACK"}, 
+		{get: {method:"JSONP"}
+	});
+
+	$scope.weatherResult = $scope.weatherAPI.get({q:$scope.city, cnt:2})
 
 	$scope.$watch('city', function() {
 		cityService.city = $scope.city;
@@ -38,7 +44,7 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope','cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource','cityService', function($scope, $resource,cityService) {
 
 	$scope.city = cityService.city;
 
