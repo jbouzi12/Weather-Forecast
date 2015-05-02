b7848d08fa039632af6c666174ab32f9d8cc5db7
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-05-02 11:23:29 -0400

    FINALLY PULLED THE API DATA

--- app.js
+++ app.js
@@ -22,29 +22,38 @@ weatherApp.config(function($routeProvider) {
 
 weatherApp.service('cityService', function() {
 
-	var self = this;
 	this.city = "New York, NY";
 
 });
 
+// Controllers
+
 weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
 
 	$scope.city = cityService.city;
-	$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', {
-		callback:"JSON_CALLBACK"}, 
-		{get: {method:"JSONP"}
-	});
-
-	$scope.weatherResult = $scope.weatherAPI.get({q:$scope.city, cnt:2})
 
 	$scope.$watch('city', function() {
+
 		cityService.city = $scope.city;
-	})
+	
+	});
 
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource','cityService', function($scope, $resource,cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource','cityService', function($scope, $resource, cityService) {
+
+	$scope.city = cityService.city;
+
+	// Weather API
+
+	var apiKey = "c5619901934de5840a17ad7d6082c1ac";
+
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+
+	$scope.weatherResult = $scope.weatherAPI.get( { q: $scope.city, cnt: 2, APPID: apiKey});
+	
+	console.log($scope.weatherResult);
 
 	$scope.city = cityService.city;
 
