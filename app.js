8e647bb2c1aab5d5a47bfc9cc6868ce96841af9a
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-05-07 12:27:09 -0400

    create factory for weather icons

--- app.js
+++ app.js
@@ -64,6 +64,28 @@ angular.module("weatherApp", ['ngRoute', 'ngResource'])
 
 }])
 
+.factory('weatherIcons', function weatherIconsFactory() {
+	return {
+		01d:'01d.png',
+		02d:'02d.png',
+		03d:'03d.png',
+		04d:'04d.png',
+		09d:'09d.png',
+		10d:'09d.png',
+		11d:'11d.png',
+		13d:'13d.png',
+		50d:'50d.png',
+		01n:'01n.png',
+		02n:'02n.png',
+		04n:'02n.png',
+		09n:'09d.png',
+		10n:'09d.png',
+		11n:'11d.png',
+		13n:'13d.png',
+		50n:'50n.png'
+	};
+})
+
 // Controllers
 
 .controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
