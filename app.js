121b0f35afa337208edb34dbe3541dd415eaeaa2
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-05-12 12:38:52 -0400

    add link function to initialize map, add id for map element

--- app.js
+++ app.js
@@ -48,7 +48,11 @@ angular.module("weatherApp", ['ngRoute', 'ngResource'])
 		template:"<div></div>",
 		replace: true,
 		link: function(scope, element, attr) {
-			
+			var mapOptions = {
+				center: new google.maps.LatLng(-34.397, 150.644),
+				zoom: 8
+			};
+			var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 		}
 
 	};
